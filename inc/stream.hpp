50a4faada7fbfd3a8c96fe2fb1a0118c7af41d64
ml-inory <584688538@qq.com>
2023-12-20 11:04:55 +0800

    remove copyright

--- inc/stream.hpp
+++ inc/stream.hpp
@@ -1,13 +1,3 @@
-/**************************************************************************************************
- *
- * Copyright (c) 2019-2023 Axera Semiconductor (Shanghai) Co., Ltd. All Rights Reserved.
- *
- * This source file is the property of Axera Semiconductor (Shanghai) Co., Ltd. and
- * may not be copied or distributed in any isomorphic form without the prior
- * written consent of Axera Semiconductor (Shanghai) Co., Ltd.
- *
- **************************************************************************************************/
-
 #pragma once
 
 #include <queue>
--- inc/string_utils.hpp
+++ inc/string_utils.hpp
@@ -1,13 +1,3 @@
-/**************************************************************************************************
- *
- * Copyright (c) 2019-2023 Axera Semiconductor (Ningbo) Co., Ltd. All Rights Reserved.
- *
- * This source file is the property of Axera Semiconductor (Ningbo) Co., Ltd. and
- * may not be copied or distributed in any isomorphic form without the prior
- * written consent of Axera Semiconductor (Ningbo) Co., Ltd.
- *
- **************************************************************************************************/
-
 #pragma once
 
 #include <string>
