11370d61dd985e96e4be5e1dc00bb57a1315258f
ml-inory <584688538@qq.com>
2023-12-21 10:53:58 +0800

    add build_rtsp.sh

---
+++ build_rtsp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+pwd=`pwd`
+third_party=${pwd}/third-party/RTSP
+install_path=${pwd}/third-party-install/RTSP
+
+if [[ ! -d ${third_party} ]]; then
+  echo "RTSP not exist, please run git submodule update"
+  exit 1
+fi
+
+# RTSP
+cd ${third_party}               
+mkdir RTSP-build
+cd RTSP-build
+cmake .. -DCMAKE_INSTALL_PREFIX=${install_path} -DCMAKE_TOOLCHAIN_FILE=${pwd}/toolchains/ax620e.cmake
+make -j8
+make install
+cd ${pwd}
